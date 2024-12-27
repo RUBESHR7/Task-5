ffe978be96281b4f3d39f0d58bdbf130100a8026
RUBESHR7 <jarvistechnology1.com>
2024-12-27 14:43:00 +0530

    Fifth task

---
+++ Assingment t-5.py
@@ -0,0 +1,104 @@
+# 1)history of python?
+ 
+#   Python was created by guido von rossam as a collage internship project in 1991, The python has three versions
+#   1989--develop--->1991--->version 0.9
+# 1994--version 1.0,2000-->version 2.0
+# 2008-->version 3.0
+# The python logo was adapted by the Tv show "mondy python circus".
+
+# 2)IDE?
+
+# An integrated development environment (IDE) is a software application that helps programmers develop software code efficiently.
+
+# 3)type of tokens?
+# Keywords, Identifiers, Literals, Operators.
+
+# # 4)datatypes?
+# #  The datatypes ib python are 
+# #   1)text-string--->str---"text"
+# # 2)number--->int,float,complex
+# # 3)boolean--->bool-->True,False
+# # 4)None---->type--->NoneType
+# # 5) Set,Tuple,List.
+# 6) Dictonary.
+
+
+
+# 5)write the programme for the output?
+# 	name: XXX
+# 	age:22
+# 	tamil:100
+# 	eng:100
+# 	maths:100
+# 	s.s:100
+# 	s:100
+# 	total:500 	and find average?
+name = "XXX"
+age = 22
+tamil = 100
+eng = 100
+maths = 100
+ss = 100  
+science = 100
+
+total=tamil+eng+maths+ss+science
+average=total/5
+
+print("Name:", name)
+print("Age:", age)
+print("Tamil:", tamil)
+print("English:", eng)
+print("Maths:", maths)
+print("Social Science:", ss)
+print("Science:", science)
+print("Total:", total)
+print("Average:", average)
+
+
+# 6)(5 & 12) & (2 | 6)
+print((5 & 12) & (2 | 6))
+# 7)(54 | 6 & 5) | (21 & 9)
+print((54 | 6 & 5) | (21 & 9))
+
+
+# 8)True or falseif it is false give the reason?
+a="name"
+a=12
+print(a)
+
+b="kalai""tamil"
+print(b)
+
+
+# 9) a=10
+#   b=2
+#   print(a/b+a%1)
+
+a=10
+b=2
+print(a/b+a%1)
+
+# 10)5>>2 and 9<<3
+print(5>>2 and 9<<3)
+
+# # 11)what is membership operator give the example?
+# Membership operator return boolean values by using in, not in.
+# eg : fruit="apple"
+# print('p' in fruit)
+
+
+# # 12)explain identity?
+# The identity it return boolen values by is or not is and it checks the memory only
+
+# # 13)output:hello hello hello hello hi hi hi hi------write a code.
+# print("hello hello hello hello hi hi hi hi")
+
+# # 14)how to find the datatype and explain?
+#  you can find datatype using type function
+#  eg: a=2
+#      print(type(a))
+
+# 15)convert the a='154' to integer?
+# # To convert string to integer
+# a='154'
+# print(int(a))
